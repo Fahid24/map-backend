9ed3688e9420f9bb84e20dfd7e79f21b98ca2eef
Fahid Hasan Fuad <fahidhasanfuad20018@gmail.com>
2023-02-10 07:51:09 +0600

    added new

--- index.js
+++ index.js
@@ -22,30 +22,25 @@ app.listen(5000, () => {
 });
 
 app.post("/api/create-checkout-session", async (req, res) => {
-  try {
-    const { product } = req.body;
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      line_items: [
-        {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: product.name,
-            },
-            unit_amount: product.price * 100,
+  const { product } = req.body;
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: product.name,
           },
-          quantity: product.quantity,
+          unit_amount: product.price * 100,
         },
-      ],
-      mode: "payment",
-      success_url:
-        "https://lighthearted-starlight-22c325.netlify.app/members/#card",
-      cancel_url: "https://lighthearted-starlight-22c325.netlify.app",
-    });
-    // res.json({ id: session.id });
-    res.json({ message: "payment successful" });
-  } catch (error) {
-    res.status(500).json({ message: "payment failed" });
-  }
+        quantity: product.quantity,
+      },
+    ],
+    mode: "payment",
+    success_url:
+      "https://lighthearted-starlight-22c325.netlify.app/members/#card",
+    cancel_url: "https://lighthearted-starlight-22c325.netlify.app",
+  });
+  res.json({ id: session.id });
 });
